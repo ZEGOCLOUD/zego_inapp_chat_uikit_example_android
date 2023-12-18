a17cb373bd33034a184e62709b43b0172ac4950c
nemo <jzyeran@gmail.com>
2023-12-18 11:35:49 +0800

    fix input config

--- ZIMKitDemo/app/src/main/java/com/example/zimkitdemo/MyApplication.java
+++ ZIMKitDemo/app/src/main/java/com/example/zimkitdemo/MyApplication.java
@@ -3,6 +3,7 @@ package com.example.zimkitdemo;
 import android.app.Application;
 
 import com.zegocloud.zimkit.services.ZIMKit;
+import com.zegocloud.zimkit.services.config.InputConfig;
 
 public class MyApplication extends Application {
     public static MyApplication sInstance;
@@ -14,5 +15,11 @@ public class MyApplication extends Application {
 
         ZIMKit.initWith(this, KeyCenter.APP_ID, KeyCenter.APP_SIGN);
         ZIMKit.initNotifications();
+
+        InputConfig inputConfig = new InputConfig();
+        inputConfig.showVoiceButton = true;
+        inputConfig.showEmojiButton = true;
+        inputConfig.showAddButton = true;
+        ZIMKit.setInputConfig(inputConfig);
     }
 }
