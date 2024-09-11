e3d52ba998cc2b3ebba083d9db25634e70f37a06
alan <alanliu@zego.im>
2024-09-11 11:29:55 +0800

    update demo

--- ZIMKitDemo/app/src/main/java/com/example/zimkitdemo/MyApplication.java
+++ ZIMKitDemo/app/src/main/java/com/example/zimkitdemo/MyApplication.java
@@ -20,13 +20,17 @@ public class MyApplication extends Application {
 
         /**
          // if you want to use call plugin,following steps(be sure you have already integrate callkit succeed):
+
          //step 1. add dependency in build.gradle
          //   implementation 'com.github.ZEGOCLOUD:zego_uikit_prebuilt_call_android:+'
+
          //step 2. add call buttons in message page.
          zimKitConfig.inputConfig.expandButtons.add(ZIMKitInputButtonName.VOICE_CALL);
          zimKitConfig.inputConfig.expandButtons.add(ZIMKitInputButtonName.VIDEO_CALL);
+
          //step 3. add call plugin config to init it.
          zimKitConfig.callPluginConfig = new ZegoCallPluginConfig();
+
          //step 4. custom call plugin config if you need(optional)
          ZegoUIKitPrebuiltCallInvitationConfig invitationConfig = new ZegoUIKitPrebuiltCallInvitationConfig();
          zimKitConfig.callPluginConfig.invitationConfig = invitationConfig;
